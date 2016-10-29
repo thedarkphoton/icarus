c4981e77033887e967180a19674ed8939d90b452
darrenjames <darren.james@kcl.ac.uk>
2016-10-29 17:02:52 +0100

    generated tree input matrix
    satisfiable predictions for now

--- demo.py
+++ demo.py
@@ -2,13 +2,35 @@ from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+# front bumber, right side, rear bumper, left side, rear lights, rear windshield, front windshield, wheels, roof , fire
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+X = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1,0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1,0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0,0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0,0],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0,0],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0,0],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0,0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0,0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0,0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0,0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1]
+    ]
+
+Y = ['write-off', 'write-off', 'write-off', 'write-off', 'write-off', 'write-off','write-off', 'write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off','not-write-off']
 
 clf = clf.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+prediction = clf.predict([ [0, 0, 1, 0, 1, 0, 0, 1, 0, 0]])
 
 print prediction
