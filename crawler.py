88081bfe86493df1ea3e99f927a426e5eab6cbfc
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2016-10-29 10:17:42 +0100

    Updated the crawler

--- crawler.py
+++ crawler.py
@@ -3,30 +3,41 @@ import xml.etree.ElementTree as ElementTree
 from os import walk
 import re
 
-data_folder = "extracted/"
+#inputs
+data_folder = "data/"
+cars = [
+    ["price", "./data/cap_data/new_prices/price/basic_price"]
+]
+claims = [
+    ["claim_no", "./{http://schemas.datacontract.org/2004/07/AudaAPI.Models}AssessmentNumber"]
+]
 
-def create_json(regexp, indicators):
+# don't modify
+def create_json(car_indicators, claim_indicators):
     f = []
+    regexp = re.compile(r'\w+.capdata.xml')
     for (dirpath, dirnames, filenames) in walk(data_folder):
         for file in filenames:
             if regexp.search(file):
-                f += [file]
+                f += [file[:file.index(".")]]
         break
 
-    json = []
+    cars = []
     for path in f:
-        data = ElementTree.parse(data_folder + path).getroot()
-        cars_json = {}
-        for car in indicators:
-            cars_json[car[0]] = data.find(car[1]).text
+        car_json = {}
 
-        json += [cars_json]
+        data = ElementTree.parse(data_folder + path + ".capdata.xml").getroot()
+        for car in car_indicators:
+            car_json[car[0]] = data.find(car[1]).text
 
-    return json
+        data = ElementTree.parse(data_folder + path + ".assessment.xml").getroot()
+        for claim in claim_indicators:
+            car_json[claim[0]] = data.find(claim[1]).text
 
-regexp = re.compile(r'\w+.capdata.xml')
-test = create_json(regexp, [
-    ["price", "./data/cap_data/new_prices/price/basic_price"]
-])
+        cars += [car_json]
+
+    return cars
+
+test = create_json(cars, claims)
 
 print(test)
