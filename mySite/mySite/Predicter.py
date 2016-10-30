0b4dca29575b0126a714b75ba190e54dfa89f47c
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2016-10-30 13:22:37 +0000

    Merge branches 'master' and 'new_form_stile' of https://github.com/TheDarkPhoton/icarus into new_form_stile
    # Conflicts:
    #	mySite/mySite/static/js/detectCar.js

--- mySite/mySite/Predicter.py
+++ mySite/mySite/Predicter.py
@@ -8,17 +8,15 @@ def predict(data):
 
     #data = json.loads(request.POST.get('data'))
 
-    formattedArray = []
-
-    for idx, val in enumerate(titles):
-        name = titles.get(idx)
-        data = data.get(name)
-        formattedArray.append(data, '')
-        print(data)
-
-    clf = joblib.load('treeClassifier_model.pkl')
-    prediction = clf.predict([formattedArray])
-    print prediction
-    return prediction
-
-predict()
+    # formattedArray = []
+    #
+    # for idx, val in enumerate(titles):
+    #     name = titles.get(idx)
+    #     data = data.get(name)
+    #     formattedArray.append(data, '')
+    #     print(data)
+    #
+    # clf = joblib.load('treeClassifier_model.pkl')
+    # prediction = clf.predict([formattedArray])
+    # print prediction
+    return ""
