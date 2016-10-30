81a3ff3b8a3a2190e9ae63a1587bbfe1d04d273d
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2016-10-30 15:22:49 +0000

    Added fire damage button

--- mySite/mySite/Predicter.py
+++ mySite/mySite/Predicter.py
@@ -6,17 +6,8 @@ titles = ['front_bumber','rear_bumper','left_side','right_side','fl_lights','fr_
 
 def predict(data):
 
-    #data = json.loads(request.POST.get('data'))
+    clf = joblib.load('treeClassifier_model.pkl')
+    predication = clf.predict(data)
 
-    # formattedArray = []
-    #
-    # for idx, val in enumerate(titles):
-    #     name = titles.get(idx)
-    #     data = data.get(name)
-    #     formattedArray.append(data, '')
-    #     print(data)
-    #
-    # clf = joblib.load('treeClassifier_model.pkl')
-    # prediction = clf.predict([formattedArray])
-    # print prediction
-    return ""
+    print predication
+    return predication
